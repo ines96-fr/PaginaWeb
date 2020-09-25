62bd80dacfe23f1391126769dbae56c01158c697
ines96-fr <62954528+ines96-fr@users.noreply.github.com>
2020-09-24 22:13:07 -0500

    Add files via upload

---
+++ configuraciones/configurar.php
@@ -0,0 +1,17 @@
+<?php
+
+define('HEADER', 'view/Plantillas/HEADER.php');
+define('SLIDER', 'view/Plantillas/SLIDER.php');
+define('FOOTER', 'view/Plantillas/FOOTER.php');
+define('MENUADM', 'view/Plantillas/MENUADM.php');
+define('MENUCLIENTE', 'view/Plantillas/MENUCLIENTE.php');
+
+define("SERVIDORBD", "localhost");
+define("PUERTO", "3306");
+define("NOMBREBD", "envueltos");
+define("USUARIO", "root");
+define("PASSWORD", "");
+
+define("SERVIDOR", "localhost");
+define("NOMBREAPP", "PracticaPatronMVC");
+define("RUTAAPP","http://".SERVIDOR."/".NOMBREAPP);
